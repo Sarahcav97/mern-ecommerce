f002b7f02067d1f0dc8de5ea8d0adc3a7cd9384c
Sarah Cavanaugh <sarahcavanaugh20@gmail.com>
2024-05-03 15:42:15 -0500

    building product page

--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -1,6 +1,55 @@
 import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import {
+	Row,
+	Col,
+	Image,
+	ListGroup,
+	Card,
+	Button,
+	ListGroupItem,
+} from 'react-bootstrap';
+import Rating from '../components/Rating';
+import { products } from '../lib/products';
 
 const ProductScreen = () => {
-	return <div>something</div>;
+	const { id } = useParams();
+
+	const product = products.find((p) => p._id === id);
+	return (
+		<>
+			{product.name}
+			<Link
+				className='btn btn-light my-3'
+				to='/'
+			>
+				Go Back
+			</Link>
+			<Row>
+				<Col md={6}>
+					<Image
+						src={product.image}
+						alt={product.name}
+						fluid
+					></Image>
+				</Col>
+				<Col md={3}>
+					<ListGroup variant='flush'>
+						<ListGroup.Item>
+							<h3>{product.name}</h3>
+						</ListGroup.Item>
+						<ListGroup.Item>
+							<Rating
+								value={product.rating}
+								text={`${product.numReviews} reviews`}
+							/>
+						</ListGroup.Item>
+						<ListGroup.Item> Price: ${product.price}</ListGroup.Item>
+						<ListGroup.Item> Description: {product.description}</ListGroup.Item>
+					</ListGroup>
+				</Col>
+			</Row>
+		</>
+	);
 };
 export default ProductScreen;
