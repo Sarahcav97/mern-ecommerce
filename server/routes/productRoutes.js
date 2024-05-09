87ef306b7c55f1a6bbe57452c42d0a37f623d02e
Sarah Cavanaugh <sarahcavanaugh20@gmail.com>
2024-05-09 16:21:05 -0500

    custom error handler

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
-import Product from '../models/productModel';
+import Product from '../models/productModel.js';
 const router = express.Router();
 
 router.get(
 	'/',
 	asyncHandler(async (req, res) => {
 		const products = await Product.find({});
+		console.log({ products });
+
 		res.json(products);
 	})
 );
@@ -17,7 +19,8 @@ router.get(
 		if (product) {
 			res.json(product);
 		} else {
-			res.json(404).json({ message: 'Product not found' });
+			res.json(404);
+			throw new Error('Product not found');
 		}
 	})
 );
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import { config } from 'dotenv';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 config();
 connectDB();
 const app = express();
@@ -13,10 +14,10 @@ app.get('/', (req, res) => {
 	res.json({ message: 'API is running' });
 });
 app.use('/api/products', productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(5000, () =>
-	console.log(
-		`server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
-	)
+	console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
