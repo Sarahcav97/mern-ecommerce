c8c6a52633d3ee28f3d637e6ad66b648cb934408
Andrew <webdev410@gmail.com>
2024-05-28 16:16:40 -0500

    remove test route

--- server/server.js
+++ server/server.js
@@ -18,9 +18,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-	res.json({ message: 'API is running' });
-});
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use(notFound);
