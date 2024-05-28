64fd63d99b9d3adaf253db90ec760985ed125996
Andrew <webdev410@gmail.com>
2024-05-28 16:32:50 -0500

    logs

--- server/server.js
+++ server/server.js
@@ -17,7 +17,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-
+app.use((req, res, next) => {
+	console.log(req.method, req.url);
+	next();
+});
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 
