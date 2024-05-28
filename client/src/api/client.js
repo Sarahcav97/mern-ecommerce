3eef20479510d7a3d64e4dad75ea415d253c2141
Andrew <webdev410@gmail.com>
2024-05-28 16:12:57 -0500

    isProd client url

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
+const isProd = window.location.href.includes('sarahcavs.com');
+const prodUrl = 'https://sarahcavs.com/api';
+const devUrl = 'http://localhost:6002/api';
+
 const client = axios.create({
-	baseURL: 'http://localhost:5000/api',
+	baseURL: isProd ? prodUrl : devUrl,
 	headers: {
 		Authorization: `Bearer ${sessionStorage.getItem('token')}`,
 	},
